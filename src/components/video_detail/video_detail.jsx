74c4899ebcbbc8e007322d7f4dee3cb35607b8b2
ohtmm <cwd05011@gmail.com>
2022-09-30 16:07:07 +0900

    Youtube video 추가

--- src/components/video_detail/video_detail.jsx
+++ src/components/video_detail/video_detail.jsx
@@ -1,11 +1,22 @@
 import React from 'react';
+import styles from './video_detail.module.css';
 
-const VideoDetail = ({selectedVideo}) => {
-    return(
-        <h2>{selectedVideo.snippet.title}</h2>
-        );
+const VideoDetail = ({selectedVideo, selectedVideo : {snippet} }) => (
+    <div className={styles.detail}>
+        <iframe 
+            type="text/html"
+            width="100%"
+            height="400px"
+            src={`https://www.youtube.com/embed/${selectedVideo.id}`}
+            frameborder="0">
+        </iframe>
+        <h2 className={styles.tit}>{snippet.title}</h2>
+        <h3 className={styles.channel}>{snippet.channelTitle}</h3>
+        <pre className={styles.desc}>
+            {snippet.description}
+        </pre>
         
 
-}
-
+    </div>
+)
 export default VideoDetail;
